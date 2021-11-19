134e5da33f3246cf13ba526a290878776782b35b
williamiuliano <williamiuliano.95@gmail.com>
2021-11-19 18:01:49 +0100

    creazione form aggiunta studente

--- src/main/java/it/nttdata/myschool/controller/StudentController.java
+++ src/main/java/it/nttdata/myschool/controller/StudentController.java
@@ -4,16 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import it.nttdata.myschool.entity.Student;
+import it.nttdata.myschool.repository.ClassroomRepository;
 import it.nttdata.myschool.repository.StudentRepository;
 
 @Controller
 public class StudentController {
 
     private StudentRepository studentRepository;
+    private ClassroomRepository classroomRepository;
 
-    public StudentController(StudentRepository studentRepository){
+    public StudentController(StudentRepository studentRepository, ClassroomRepository classroomRepository){
         this.studentRepository = studentRepository;
+        this.classroomRepository = classroomRepository;
     }
 
     @GetMapping("/students")
@@ -29,6 +34,20 @@ public class StudentController {
         model.addAttribute("title", "Lista Studenti My School " + section );
         return "students";
         
+    }
+
+    @PostMapping("/addStudents")
+    public String addStudent(Student student){
+        studentRepository.save(student);
+        return "redirect:/students";
+
+    }
+
+    @GetMapping("/addStudents")
+    public String getNewStudent(Model model){
+        model.addAttribute("classrooms", classroomRepository.findAll());
+        return "addStudents";
+
     }
     
 }
